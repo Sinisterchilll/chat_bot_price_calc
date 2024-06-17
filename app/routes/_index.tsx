9b418156869a1517456ad0e6860bc4ac2472dd06
Sinisterchilll <anucbs22@gmail.com>
2024-06-18 00:10:56 +0530

    added responsiveness

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             </p>
 
           </div>
-          <div className="flex h-auto w-full mt-12">
+          <div className="flex flex-col lg:flex-row h-auto w-full mt-12">
             <div className="flex-1 bg-gradient-to-b from-gray-100 to-white p-4 rounded-lg">
               <div className="flex justify-between">
                 <div className='grow'>
