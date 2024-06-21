a2375c745014646e25a31d4fd3e5918214f0f6a8
Sinisterchilll <anucbs22@gmail.com>
2024-06-21 21:48:15 +0530

    optimized formula

--- app/routes/calculator.tsx
+++ app/routes/calculator.tsx
@@ -31,6 +31,20 @@ const Calculator: React.FC<CalculatorProps> = ({
     return agents * hours * costPerHour;
   };
 
+  // const calculateSavings = (): number => {
+  //   const monthlyAgentCost = agents * hours * costPerHour;
+  //   const ticketsResolvedByhuman = tickets * (1 - autoResolved / 100);
+  //   const timetaken = ticketsResolvedByhuman * (resolutionTime / 60);
+  //   const costhumanwithai = (timetaken * costPerHour) / agents;
+  //   return costhumanwithai;
+  // };
+
+  // const calculateExpenseBefore = (): number => {
+  //   const totaltime = tickets * (resolutionTime/60) ;
+  //   const costhuman = (totaltime * costPerHour) / agents ; 
+  //   return costhuman
+  // };
+
   const expenseBefore = calculateExpenseBefore();
   const estimatedSavings = calculateSavings();
 
@@ -46,16 +60,17 @@ const Calculator: React.FC<CalculatorProps> = ({
             className="w-16 bg-gray-200 rounded-md"
             style={{ height: `calc(${expenseHeight}px + 10px)` }}
           ></div>
-          <span className="mt-2 text-gray-800">Cost with Agents</span>
-          <span className="text-red-600 font-bold">${expenseBefore.toFixed(2)}</span>
+          {/* <span className="mt-2 text-gray-800">Cost with Agents</span> */}
+          <span className="text-red-600 font-bold mt-2">${expenseBefore.toFixed(2)}<span className='text-gray-400'>/month</span><p className='text-blue-600 mx-2 font-semibold'><span className='text-gray-400'>without</span> siteGPT</p></span>
         </div>
+        <span className='font-bold mb-20'>or</span>
         <div className="flex flex-col items-center">
           <div
             className="w-16 bg-blue-600 rounded-md"
             style={{ height: `calc(${savingsHeight}px + 10px)` }}
           ></div>
-          <span className="mt-2 text-gray-800">Estimated Savings</span>
-          <span className="text-green-600 font-bold">${estimatedSavings.toFixed(2)}</span>
+          {/* <span className="mt-2 text-gray-800">Estimated Savings</span> */}
+          <span className="text-green-600 font-bold mt-2">${estimatedSavings.toFixed(2)}<span className='text-gray-400'>/month</span><p className='text-blue-600 mx-6 font-semibold'><span className='text-gray-400'>with</span> siteGPT</p></span>
         </div>
       </div>
     </div>
