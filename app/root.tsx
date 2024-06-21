4cc5f5e98be3ac6883660b0d8180f5d44a46dab6
Sinisterchilll <anucbs22@gmail.com>
2024-06-21 13:52:42 +0530

    improved ui

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,7 @@ import type { LinksFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
 
 
+
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,7 @@
-import { useState, useEffect} from 'react';
+import { useState } from 'react';
 import Calculator from './calculator';
 
-
 export default function Home() {
-  // State to hold slider values
   const [sliderValue1, setSliderValue1] = useState(25);
   const [sliderValue2, setSliderValue2] = useState(25);
   const [sliderValue3, setSliderValue3] = useState(25);
@@ -11,36 +9,36 @@ export default function Home() {
   const [sliderValue5, setSliderValue5] = useState(25);
   const [sliderValue6, setSliderValue6] = useState(25);
 
-  const handleInputChange = (e:any, setSliderValue:any) => {
+  const handleInputChange = (e: any, setSliderValue: any) => {
     const value = parseInt(e.target.value);
     if (!isNaN(value) || e.target.value === '') {
       setSliderValue(value);
     }
   };
 
- 
-
   return (
     <>
-      <section className="bg-gradient-to-b from-white to-blue-50 py-12 sm:py-16 lg:py-20 xl:py-24">
-        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <section className="py-12 bg-gradient-to-b from-white to-blue-50 sm:py-16 lg:py-20 xl:py-24">
+        <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
           <div className="max-w-xl mx-auto text-center lg:max-w-4xl">
-
-            <p className="font-display text-4xl font-bold tracking-tight text-gray-900 mt-5 sm:text-8xl pt-20 pb-5 lg:text-6xl">
+            <h1 className="text-base font-bold tracking-widest text-blue-600 uppercase">
+              TOOLS
+            </h1>
+            <p className="mt-5 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl font-display">
               Chatbot cost calculator
             </p>
-            <p className="max-w-2xl text-base font-normal text-gray-700 mx-auto mt-5 sm:text-3xl pt-5 pb-10 lg:text-2xl">
-              Are you looking to adopt a chatbot but are unsure what it will cost?
-              Dont't worry you are at the right place
+            <p className="max-w-3xl mx-auto mt-5 text-base font-normal text-gray-700 sm:text-lg lg:text-xl">
+              Are you looking to adopt a chatbot but are unsure what it will cost? Don’t worry you are at the right place
             </p>
-
           </div>
-          <div className="flex flex-col lg:flex-row h-auto w-full pt-12">
-            <div className="flex-1 bg-gradient-to-b from-gray-100 to-white p-4 rounded-lg">
-              <div className="flex justify-between">
-                <div className='grow'>
-                  <label htmlFor="slider1" className="block text-sm font-normal text-gray-700 sm:text-3xl lg:text-xl ">Number of Customer Service Agents:</label>
-                  <input 
+
+          <div className="max-w-6xl mx-auto mt-16 lg:mt-20 flex flex-col lg:flex-row border rounded-3xl border-gray-300 overflow-hidden">
+            {/* TOOL CONFIGURATIONS */}
+            <div className="p-8 bg-gray-50 w-full flex flex-col gap-8">
+              <div className="flex justify-between gap-6 w-full items-center">
+                <div className="w-full">
+                  <label className="block mb-2 text-sm font-medium text-gray-700">How many agents do you have?</label>
+                  <input
                     type="range"
                     id="slider1"
                     name="slider1"
@@ -48,32 +46,26 @@ export default function Home() {
                     max="100"
                     value={sliderValue1}
                     onChange={(e) => setSliderValue1(parseInt(e.target.value))}
-                    className="block w-full h-3 mt-6 bg-gray-200 rounded-md  accent-blue-500 "
-                    
+                    className="w-full range-slider h-2 bg-gray-200 rounded-lg accent-blue-600 cursor-pointer"
                   />
-
-
-                  <div className="flex justify-between mt-2">
-                    <span>0</span>
-                    <span>50</span>
-                    <span>100</span>
+                  <div className="w-full flex justify-between mt-2">
+                    <span className="text-sm text-gray-400 font-medium">50</span>
+                    <span className="text-sm text-gray-400 font-medium">100</span>
+                    <span className="text-sm text-gray-400 font-medium">150</span>
+                    <span className="text-sm text-gray-400 font-medium">200</span>
                   </div>
                 </div>
-
-                <div className=" mt-11 mx-5">
                 <input
-                    type="number"
-                    value={sliderValue1}
-                    onChange={(e) => handleInputChange(e, setSliderValue1)}
-                    className=" py-1 bg-white rounded-md border border-blue-600 w-12 pl-3 "
-                  />
-                </div>
+                  type="number"
+                  value={sliderValue1}
+                  onChange={(e) => handleInputChange(e, setSliderValue1)}
+                  className="w-16 h-9 rounded-lg border border-blue-600 text-center"
+                />
               </div>
 
-
-              <div className="flex justify-between">
-                <div className='grow'>
-                  <label htmlFor="slider1" className="block text-sm font-normal text-gray-700 sm:text-3xl lg:text-xl ">Monthly working hours (per agent):</label>
+              <div className="flex justify-between gap-6 w-full items-center">
+                <div className="w-full">
+                  <label className="block mb-2 text-sm font-medium text-gray-700">How many hours do they work per month/week/day?</label>
                   <input
                     type="range"
                     id="slider2"
@@ -82,31 +74,26 @@ export default function Home() {
                     max="100"
                     value={sliderValue2}
                     onChange={(e) => setSliderValue2(parseInt(e.target.value))}
-                    className="block w-full h-3 mt-6 bg-gray-200 rounded-md accent-blue-500 "
+                    className="w-full range-slider h-2 bg-gray-200 rounded-lg accent-blue-600 cursor-pointer"
                   />
-
-
-                  <div className="flex justify-between mt-2">
-                    <span>0</span>
-                    <span>50</span>
-                    <span>100</span>
+                  <div className="w-full flex justify-between mt-2">
+                    <span className="text-sm text-gray-400 font-medium">50</span>
+                    <span className="text-sm text-gray-400 font-medium">100</span>
+                    <span className="text-sm text-gray-400 font-medium">150</span>
+                    <span className="text-sm text-gray-400 font-medium">200</span>
                   </div>
                 </div>
-
-                <div className=" mt-11 mx-5">
                 <input
-                    type="number"
-                    value={sliderValue2}
-                    onChange={(e) => handleInputChange(e, setSliderValue2)}
-                    className=" py-1 bg-white rounded-md border border-blue-600 w-12 pl-3 "
-                  />
-                </div>
+                  type="number"
+                  value={sliderValue2}
+                  onChange={(e) => handleInputChange(e, setSliderValue2)}
+                  className="w-16 h-9 rounded-lg border border-blue-600 text-center"
+                />
               </div>
 
-
-              <div className="flex justify-between">
-                <div className='grow'>
-                  <label htmlFor="slider1" className="block text-sm font-normal text-gray-700 sm:text-3xl lg:text-xl ">Salary per hour (in $ per agent):</label>
+              <div className="flex justify-between gap-6 w-full items-center">
+                <div className="w-full">
+                  <label className="block mb-2 text-sm font-medium text-gray-700">What is the cost per hour per agent?</label>
                   <input
                     type="range"
                     id="slider3"
@@ -115,30 +102,26 @@ export default function Home() {
                     max="100"
                     value={sliderValue3}
                     onChange={(e) => setSliderValue3(parseInt(e.target.value))}
-                    className="block w-full h-3 mt-6 bg-gray-200 rounded-md  accent-blue-500"
+                    className="w-full range-slider h-2 bg-gray-200 rounded-lg accent-blue-600 cursor-pointer"
                   />
-
-
-                  <div className="flex justify-between mt-2">
-                    <span>0</span>
-                    <span>50</span>
-                    <span>100</span>
+                  <div className="w-full flex justify-between mt-2">
+                    <span className="text-sm text-gray-400 font-medium">50</span>
+                    <span className="text-sm text-gray-400 font-medium">100</span>
+                    <span className="text-sm text-gray-400 font-medium">150</span>
+                    <span className="text-sm text-gray-400 font-medium">200</span>
                   </div>
                 </div>
-
-                <div className=" mt-11 mx-5">
                 <input
-                    type="number"
-                    value={sliderValue3}
-                    onChange={(e) => handleInputChange(e, setSliderValue3)}
-                    className=" py-1 bg-white rounded-md border border-blue-600 w-12 pl-3 "
-                  />
-                </div>
+                  type="number"
+                  value={sliderValue3}
+                  onChange={(e) => handleInputChange(e, setSliderValue3)}
+                  className="w-16 h-9 rounded-lg border border-blue-600 text-center"
+                />
               </div>
 
-              <div className="flex justify-between">
-                <div className='grow'>
-                  <label htmlFor="slider1" className="block text-sm font-normal text-gray-700 sm:text-3xl lg:text-xl ">How many Tickets do you get each month:</label>
+              <div className="flex justify-between gap-6 w-full items-center">
+                <div className="w-full">
+                  <label className="block mb-2 text-sm font-medium text-gray-700">How many tickets do you get each month?</label>
                   <input
                     type="range"
                     id="slider4"
@@ -147,30 +130,26 @@ export default function Home() {
                     max="100"
                     value={sliderValue4}
                     onChange={(e) => setSliderValue4(parseInt(e.target.value))}
-                    className="block w-full h-3 mt-6 bg-gray-200 rounded-md accent-blue-500"
+                    className="w-full range-slider h-2 bg-gray-200 rounded-lg accent-blue-600 cursor-pointer"
                   />
-
-
-                  <div className="flex justify-between mt-2">
-                    <span>0</span>
-                    <span>50</span>
-                    <span>100</span>
+                  <div className="w-full flex justify-between mt-2">
+                    <span className="text-sm text-gray-400 font-medium">50</span>
+                    <span className="text-sm text-gray-400 font-medium">100</span>
+                    <span className="text-sm text-gray-400 font-medium">150</span>
+                    <span className="text-sm text-gray-400 font-medium">200</span>
                   </div>
                 </div>
-
-                <div className=" mt-11 mx-5">
                 <input
-                    type="number"
-                    value={sliderValue4}
-                    onChange={(e) => handleInputChange(e, setSliderValue4)}
-                    className=" py-1 bg-white rounded-md border border-blue-600 w-12 pl-3 "
-                  />
-                </div>
+                  type="number"
+                  value={sliderValue4}
+                  onChange={(e) => handleInputChange(e, setSliderValue4)}
+                  className="w-16 h-9 rounded-lg border border-blue-600 text-center"
+                />
               </div>
 
-              <div className="flex justify-between">
-                <div className='grow'>
-                  <label htmlFor="slider1" className="block text-sm font-normal text-gray-700 sm:text-3xl lg:text-xl ">Time it takes to resolve each Ticket (in mins):</label>
+              <div className="flex justify-between gap-6 w-full items-center">
+                <div className="w-full">
+                  <label className="block mb-2 text-sm font-medium text-gray-700">Time it takes to resolve each ticket (in mins)</label>
                   <input
                     type="range"
                     id="slider5"
@@ -179,30 +158,26 @@ export default function Home() {
                     max="100"
                     value={sliderValue5}
                     onChange={(e) => setSliderValue5(parseInt(e.target.value))}
-                    className="block w-full h-3 mt-6 bg-gray-200 rounded-md accent-blue-500"
+                    className="w-full range-slider h-2 bg-gray-200 rounded-lg accent-blue-600 cursor-pointer"
                   />
-
-
-                  <div className="flex justify-between mt-2">
-                    <span>0</span>
-                    <span>50</span>
-                    <span>100</span>
+                  <div className="w-full flex justify-between mt-2">
+                    <span className="text-sm text-gray-400 font-medium">50</span>
+                    <span className="text-sm text-gray-400 font-medium">100</span>
+                    <span className="text-sm text-gray-400 font-medium">150</span>
+                    <span className="text-sm text-gray-400 font-medium">200</span>
                   </div>
                 </div>
-
-                <div className=" mt-11 mx-5">
                 <input
-                    type="number"
-                    value={sliderValue5}
-                    onChange={(e) => handleInputChange(e, setSliderValue5)}
-                    className=" py-1 bg-white rounded-md border border-blue-600 w-12 pl-3 "
-                  />
-                </div>
+                  type="number"
+                  value={sliderValue5}
+                  onChange={(e) => handleInputChange(e, setSliderValue5)}
+                  className="w-16 h-9 rounded-lg border border-blue-600 text-center"
+                />
               </div>
 
-              <div className="flex justify-between">
-                <div className='grow'>
-                  <label htmlFor="slider1" className="block text-sm font-normal text-gray-700 sm:text-3xl lg:text-xl ">Percentage of Tickets resolved by AI:</label>
+              <div className="flex justify-between gap-6 w-full items-center">
+                <div className="w-full">
+                  <label className="block mb-2 text-sm font-medium text-gray-700">% of tickets you want AI to resolve automatically</label>
                   <input
                     type="range"
                     id="slider6"
@@ -211,42 +186,37 @@ export default function Home() {
                     max="100"
                     value={sliderValue6}
                     onChange={(e) => setSliderValue6(parseInt(e.target.value))}
-                    className="block w-full h-3 mt-6 bg-gray-200 rounded-md accent-blue-500"
+                    className="w-full range-slider h-2 bg-gray-200 rounded-lg accent-blue-600 cursor-pointer"
                   />
-
-
-                  <div className="flex justify-between mt-2">
-                    <span>0</span>
-                    <span>50</span>
-                    <span>100</span>
+                  <div className="w-full flex justify-between mt-2">
+                    <span className="text-sm text-gray-400 font-medium">50</span>
+                    <span className="text-sm text-gray-400 font-medium">100</span>
+                    <span className="text-sm text-gray-400 font-medium">150</span>
+                    <span className="text-sm text-gray-400 font-medium">200</span>
                   </div>
                 </div>
-
-                <div className=" mt-11 mx-5">
                 <input
-                    type="number"
-                    value={sliderValue6}
-                    onChange={(e) => handleInputChange(e, setSliderValue6)}
-                    className=" py-1 bg-white rounded-md border border-blue-600 w-12 pl-3 "
-                  />
-                </div>
+                  type="number"
+                  value={sliderValue6}
+                  onChange={(e) => handleInputChange(e, setSliderValue6)}
+                  className="no-spinner w-16 h-9 rounded-lg border border-blue-600 text-center"
+                />
               </div>
-
             </div>
 
-            <div className="flex-1 bg-white ">
+            <div className="p-8 bg-white w-full order-2 lg:order-none">
               <Calculator
-              agents={sliderValue1}
-              hours={sliderValue2}
-              costPerHour={sliderValue3}
-              tickets={sliderValue4}
-              autoResolved={sliderValue5}
-              resolutionTime={sliderValue6}
-            /> 
+                agents={sliderValue1}
+                hours={sliderValue2}
+                costPerHour={sliderValue3}
+                tickets={sliderValue4}
+                autoResolved={sliderValue5}
+                resolutionTime={sliderValue6}
+              />
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
